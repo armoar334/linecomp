79b2d90cc762418e70af6dc414c6575e99db7929
armoar334 <alfiecgordon@gmail.com>
2023-02-16 12:40:55 +0000

    Fix history suggestion

--- linecomp.sh
+++ linecomp.sh
@@ -65,7 +65,7 @@ search_escape() {
 }
 
 history_completion() {
-	history_args=$(cat "$HISTFILE" | tac | grep -m1 -F "$string")
+	history_args=$(cat "$HISTFILE" | tac | grep -m1 '^'"$string")
 	history_args="${history_args%%$'\n'*}"
 	rem_str="${string% *}"
 	history_args="${history_args/$rem_str}"
