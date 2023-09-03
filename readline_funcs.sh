035605e706021481f5853a6873cbd175584794b0
armoar334 <alfiecgordon@gmail.com>
2023-09-03 23:45:42 +0000

    Temporary fix for issue #6

--- readline_funcs.sh
+++ readline_funcs.sh
@@ -159,8 +159,11 @@ kill-word() {
 next-history() {
 	((_comp_hist-=1))
 	if [[ $_comp_hist -le 0 ]]; then _comp_hist=0; fi
-	READLINE_LINE="$(history_get)"
-	READLINE_POINT=${#READLINE_LINE}
+	if [[ "$READLINE_LINE" != *$'\n'* ]]
+	then
+		READLINE_LINE="$(history_get)"
+		READLINE_POINT=${#READLINE_LINE}
+	fi
 }
 #next-screen-line
 #non-incremental-forward-search-history
@@ -187,8 +190,11 @@ previous-history() {
 	then
 		_comp_hist=$_histmax
 	fi
-	READLINE_LINE="$(history_get)"
-	READLINE_POINT=${#READLINE_LINE}
+	if [[ "$READLINE_LINE" != *$'\n'* ]]
+	then
+		READLINE_LINE="$(history_get)"
+		READLINE_POINT=${#READLINE_LINE}
+	fi
 }
 #previous-screen-line
 #print-last-kbd-macro
